5d67273f828cdab0b874dea6be6c6cd515142794
Martín Acosta <tinchoz49@gmail.com>
2018-10-22 17:28:29 -0300

    Added gql service.

---
+++ @datalab/backend/data.js
@@ -0,0 +1,108 @@
+export default {
+  columns: [
+    {
+      id: 'column-1',
+      __typename: 'Column',
+      title: 'column 1',
+      cards: [
+        {
+          id: 'card-1',
+          __typename: 'Card',
+          title: 'card-1'
+        },
+        {
+          id: 'card-2',
+          __typename: 'Card',
+          title: 'card-2'
+        },
+        {
+          id: 'card-3',
+          __typename: 'Card',
+          title: 'card-3'
+        }
+      ]
+    },
+    {
+      id: 'column-2',
+      __typename: 'Column',
+      title: 'column 2',
+      cards: [
+        {
+          id: 'card-21',
+          __typename: 'Card',
+          title: 'card-21'
+        },
+        {
+          id: 'card-22',
+          __typename: 'Card',
+          title: 'card-22'
+        },
+        {
+          id: 'card-23',
+          __typename: 'Card',
+          title: 'card-23'
+        }
+      ]
+    },
+    {
+      id: 'column-3',
+      __typename: 'Column',
+      title: 'column 3',
+      cards: [
+        {
+          id: 'card-31',
+          __typename: 'Card',
+          title: 'card-31'
+        },
+        {
+          id: 'card-32',
+          __typename: 'Card',
+          title: 'card-32'
+        },
+        {
+          id: 'card-33',
+          __typename: 'Card',
+          title: 'card-33'
+        }
+      ]
+    }
+  ],
+  services: [
+    {
+      id: 'service-1',
+      __typename: 'Service',
+      type: 'messaging',
+      name: 'Google Mail',
+      description: 'Service to send messages through Google Mail.',
+      enabled: false,
+      url: 'https://6jl9mysuhl.execute-api.us-east-1.amazonaws.com/dev/gql'
+    },
+    {
+      id: 'service-2',
+      __typename: 'Service',
+      type: 'tasks',
+      name: 'Asana',
+      description: 'Create tasks connected to Asana.',
+      enabled: false,
+      url: 'https://3fkxqlidl0.execute-api.us-east-1.amazonaws.com/dev/gql'
+    },
+    {
+      id: 'service-3',
+      __typename: 'Service',
+      type: 'contacts',
+      name: 'LinkedIn',
+      description: 'Check your contacts from LinkedIn.',
+      enabled: false,
+      url: null
+    },
+    {
+      id: 'service-4',
+      __typename: 'Service',
+      type: 'messaging',
+      name: 'Hotmail Mail',
+      description: 'Service to send messages through Hotmail.',
+      enabled: false,
+      url: 'https://tzd6hneuuk.execute-api.us-east-1.amazonaws.com/dev/gql'
+    }
+  ]
+};
---
+++ @datalab/backend/handler.js
@@ -0,0 +1,66 @@
+//
+// Copyright 2017 Wireline, Inc.
+//
+
+// Enable source map support.
+// https://github.com/evanw/node-source-map-support#programmatic-usage
+import 'source-map-support/register';
+
+import Wireline from '@wirelineio/sdk';
+import { concatenateTypeDefs, makeExecutableSchema } from 'graphql-tools';
+import { graphql, GraphQLScalarType } from 'graphql';
+import { Kind } from 'graphql/language';
+
+import SourceSchema from './schema.graphql';
+
+import { columns, services } from './data';
+
+const schema = makeExecutableSchema({
+  // Schema types.
+  typeDefs: concatenateTypeDefs([SourceSchema]),
+
+  // http://dev.apollodata.com/tools/graphql-tools/resolvers.html
+  resolvers: {
+    Query: {
+      getAllColumns() {
+        return columns;
+      },
+      getAllServices() {
+        return services;
+      }
+    },
+    Mutation: {
+      //addTask(obj, { content, to }) {
+
+      //},
+    },
+    Date: new GraphQLScalarType({
+      name: 'Date',
+      description: 'Date custom scalar type',
+      parseValue(value) {
+        return new Date(value);
+      },
+      serialize(value) {
+        return value.getTime();
+      },
+      parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+          return new Date(ast.value);
+        }
+        return null;
+      }
+    })
+  }
+});
+
+module.exports = {
+  gql: Wireline.exec(async (event, context, response) => {
+    const { body } = event;
+    const { query, variables } = typeof body === 'string' ? JSON.parse(body) : body;
+    let queryRoot = {};
+    let queryContext = {};
+
+    const { errors, data } = await graphql(schema, query, queryRoot, queryContext, variables);
+    response.send({ data, errors });
+  })
+};
