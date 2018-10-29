e307e2cd383d03b05fe82f8f1adefc9d6ffc16da
Martín Acosta <tinchoz49@gmail.com>
2018-10-29 13:35:20 -0300

    Updated graphql version.

--- services/contacts/handler.js
+++ services/contacts/handler.js
@@ -26,17 +26,90 @@ const schema = makeExecutableSchema({
   resolvers: {
     Query: {
       async getAllContacts(obj, args, { store }) {
-        const { contacts = [] } = await store.get('contacts');
+        const [{ contacts = [] }, { companies = [] }, { areas = [] }] = await Promise.all([
+          store.get('contacts'),
+          store.get('companies'),
+          store.get('areas')
+        ]);
         contacts.sort((a, b) => a.createdAt - b.createdAt);
-        return contacts;
+        return contacts.map(c => {
+          c.company = companies.find(comp => comp.id === c.companyId);
+          c.area = areas.find(a => a.id === c.areaId);
+          return c;
+        });
+      },
+      async getContact(obj, { id }, { store }) {
+        const [{ contacts = [] }, { companies = [] }, { areas = [] }] = await Promise.all([
+          store.get('contacts'),
+          store.get('companies'),
+          store.get('areas')
+        ]);
+        const contact = contacts.find(c => c.id === id);
+        contact.company = companies.find(comp => comp.id === contact.companyId);
+        contact.area = areas.find(a => a.id === contact.areaId);
+        return contact;
+      },
+      async getAllCompanies(obj, args, { store }) {
+        const { companies = [] } = await store.get('companies');
+        return companies;
+      },
+      async getAllAreas(obj, args, { store }) {
+        const { areas = [] } = await store.get('areas');
+        return areas;
       }
     },
     Mutation: {
-      sendMessage(obj, { content, to }) {
-        messageIdx++;
-        const message = { id: `message-${messageIdx}`, content, from: 'tincho', to, createdAt: new Date() };
-        messages.push(message);
-        return message;
+      async addContact(obj, args, { store }) {
+        const { contacts = [] } = await store.get('contacts');
+        const contact = Object.assing({ id: uuid() }, args);
+        contacts.push(contact);
+        await store.set('contacts', contacts);
+        return contact;
+      },
+      async updateContact(obj, args, { store }) {
+        const { id } = args;
+
+        const { contacts = [] } = await store.get('contacts');
+        const idx = contacts.findIndex(c => c.id === id);
+        const contact = contacts[idx];
+
+        if (!contact) {
+          return null;
+        }
+
+        Object.keys(args).forEach(prop => {
+          if (prop !== 'id') {
+            contact[prop] = args[prop];
+          }
+        });
+
+        await store.set('contacts', contacts);
+        return contact;
+      },
+      async deleteContact(obj, { id }, { store }) {
+        const { contacts = [] } = await store.get('contacts');
+        const contact = contacts.find(c => c.id === id);
+
+        if (!contact) {
+          return null;
+        }
+
+        await store.set('contacts', contacts.filter(c => c.id !== id));
+        return contact;
+      },
+      async addCompany(obj, { name }, { store }) {
+        const { companies = [] } = await store.get('companies');
+        const company = { id: uuid(), name };
+        companies.push(company);
+        await store.set('companies', companies);
+        return company;
+      },
+      async addArea(obj, { name }, { store }) {
+        const { areas = [] } = await store.get('areas');
+        const area = { id: uuid(), name };
+        areas.push(area);
+        await store.set('areas', areas);
+        return area;
       }
     },
     Date: new GraphQLScalarType({
@@ -64,7 +137,7 @@ module.exports = {
     const { query, variables } = typeof body === 'string' ? JSON.parse(body) : body;
     let queryRoot = {};
 
-    const store = new Store(context);
+    const store = new Store(context, { bucket: 'dl-contacts' });
     let queryContext = {
       store
     };
