4ccc47d9b31cb6750c7dc8cb5e051f570e0a74ac
Max Fierro <elmasse@gmail.com>
2018-10-30 16:07:45 -0300

    Change App title.

--- @datalab/app/handler.js
+++ @datalab/app/handler.js
@@ -23,7 +23,7 @@ export const index = Wireline.exec(async (event, context, response) => {
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
       />
-      <title>Wireline Github App</title>
+      <title>DataLab</title>
       <link rel="shortcut icon" href="${process.env.PUBLIC_PATH}/favicon.ico">
       <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     </head>
