d57a5213d05c660bcc69e0faf5b52a37f217d154
Martín Acosta <tinchoz49@gmail.com>
2018-11-22 13:57:06 -0300

    Added combo stage. (#14)

--- @datalab/app/src/components/modal/PartnerForm.js
+++ @datalab/app/src/components/modal/PartnerForm.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -11,12 +12,21 @@ import { Formik, Field } from 'formik';
 import * as Yup from 'yup';
 
 import TextField from '../form/TextField';
+import Autocomplete from '../form/Autocomplete';
 import RichText from '../editor/RichText';
+
+const toStageOption = c => {
+  if (c) {
+    return { label: c.name, value: c.id };
+  }
+
+  return { label: 'Uncategorized', value: null };
+};
 
 const initialValues = (partner, stage) => ({
   id: partner ? partner.id : null,
   name: partner ? partner.name : '',
-  stageId: partner ? partner.stageId : stage ? stage.id : null,
+  stage: partner ? toStageOption(partner.stage) : toStageOption(stage),
   url: partner ? partner.url || '' : '',
   goals: partner ? partner.goals || '' : ''
 });
@@ -26,10 +36,23 @@ const validationSchema = Yup.object().shape({
     .min(3, 'Name must be at least 3 characters long.')
     .required('Name is required.'),
   url: Yup.string().url('The Website must be a valid url.'),
-  goals: Yup.string()
+  goals: Yup.string(),
+  stage: Yup.object()
+    .shape({
+      label: Yup.string(),
+      value: Yup.string().nullable()
+    })
+    .nullable()
+    .required('Stage is required')
 });
 
-export default class PartnerForm extends Component {
+const styles = theme => ({
+  autocomplete: {
+    marginTop: theme.spacing.unit
+  }
+});
+
+class PartnerForm extends Component {
   handleClose = () => {
     const { onClose } = this.props;
     onClose(false);
@@ -42,7 +65,9 @@ export default class PartnerForm extends Component {
   };
 
   render() {
-    const { open, partner, stage, onSpellcheck } = this.props;
+    const { open, partner, stage, stages = [], onSpellcheck, classes } = this.props;
+
+    const stagesOptions = stages.map(toStageOption);
 
     return (
       <Dialog open={open} onClose={this.handleClose} aria-labelledby="form-dialog-title" fullWidth>
@@ -56,6 +81,14 @@ export default class PartnerForm extends Component {
             <Fragment>
               <DialogContent>
                 <form onSubmit={props.handleSubmit}>
+                  <Field
+                    name="stage"
+                    component={Autocomplete}
+                    className={classes.autocomplete}
+                    options={stagesOptions}
+                    placeholder="Stage..."
+                    textFieldProps={{ margin: 'dense' }}
+                  />
                   <Field component={TextField} autoFocus margin="dense" name="name" label="Name" fullWidth />
                   <Field component={TextField} margin="dense" name="url" label="Website" fullWidth />
                   <Field
@@ -82,3 +115,5 @@ export default class PartnerForm extends Component {
     );
   }
 }
+
+export default withStyles(styles)(PartnerForm);
--- @datalab/app/src/containers/Partners.js
+++ @datalab/app/src/containers/Partners.js
@@ -125,7 +125,7 @@ class Partners extends Component {
         name: result.name,
         url: result.url.length > 0 ? result.url : null,
         goals: result.goals.length > 0 ? result.goals : null,
-        stageId: result.stageId
+        stageId: result.stage.value
       };
 
       if (result.id) {
@@ -247,6 +247,7 @@ class Partners extends Component {
           open={openPartnerForm}
           partner={selectedPartner}
           stage={selectedStage}
+          stages={[{ id: null, name: 'Uncategorized' }, ...stages]}
           onClose={this.handlePartnerFormResult}
           onSpellcheck={this.handleSpellcheck}
         />
