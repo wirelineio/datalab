3d834edcdf60456a0300d90947b335786a076edb
Esteban Primost <esteban.primost@gmail.com>
2018-11-27 11:33:46 -0300

    Gql changes

--- @datalab/native/src/containers/Partners.js
+++ @datalab/native/src/containers/Partners.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { compose, graphql, withApollo } from 'react-apollo';
 import { GET_ALL_PARTNERS } from '../stores/orgs';
 import { GET_SERVICES, getType } from '../stores/services';
@@ -7,12 +7,6 @@ import List from '../components/partners/List';
 
 const Partners = props => {
   const { partners = [] } = props;
-
-  // const partners = Array.from({ length: 40 }).map((_, i) => ({
-  //   id: i,
-  //   name: `Pirlo ${i}`,
-  //   extra: `Soy un subtítulo para Pirlo ${i}`
-  // }));
 
   return (
     <View style={styles.container}>
@@ -35,6 +29,11 @@ const styles = StyleSheet.create({
 export default compose(
   withApollo,
   graphql(GET_SERVICES, {
+    options: {
+      context: {
+        useNetworkStatusNotifier: false
+      }
+    },
     props({ data: { services = [] }, ownProps: { client } }) {
       services = services.map(s => ({ ...s, type: getType(s) }));
 
@@ -43,15 +42,6 @@ export default compose(
     }
   }),
   graphql(GET_ALL_PARTNERS, {
-    skip({ services = [] }) {
-      return !services.find(s => s.type === 'orgs');
-    },
-    options: {
-      context: {
-        serviceType: 'orgs'
-      },
-      fetchPolicy: 'cache-and-network'
-    },
     props({ data: { partners = [], stages = [], loading } }) {
       return {
         partners,
