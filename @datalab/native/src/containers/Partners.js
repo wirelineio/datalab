f54071bdb0132af99754e30442144e8886b05a83
Esteban Primost <esteban.primost@gmail.com>
2018-12-04 10:00:24 -0300

    Back to default partners from service

--- @datalab/native/src/containers/Partners.js
+++ @datalab/native/src/containers/Partners.js
@@ -6,13 +6,7 @@ import { GET_SERVICES, getType } from '../stores/services';
 import List from '../components/partners/List';
 
 const Partners = props => {
-  // const { partners = [] } = props;
-
-  const partners = Array.from({ length: 30 }).map((_, i) => ({
-    id: i,
-    name: `Pirlo ${i}`,
-    url: `http://pirlo${i}.com`
-  }));
+  const { partners = [] } = props;
 
   return (
     <View style={styles.container}>
@@ -32,33 +26,31 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Partners;
+export default compose(
+  withApollo,
+  graphql(GET_SERVICES, {
+    options: {
+      context: {
+        useNetworkStatusNotifier: false
+      }
+    },
+    props({ data: { services = [] }, ownProps: { client } }) {
+      services = services.map(s => ({ ...s, type: getType(s) }));
 
-// export default compose(
-//   withApollo,
-//   graphql(GET_SERVICES, {
-//     options: {
-//       context: {
-//         useNetworkStatusNotifier: false
-//       }
-//     },
-//     props({ data: { services = [] }, ownProps: { client } }) {
-//       services = services.map(s => ({ ...s, type: getType(s) }));
-
-//       client.updateServices(services);
-//       return { services: services.filter(s => s.enabled) };
-//     }
-//   }),
-//   graphql(GET_ALL_PARTNERS, {
-//     options: {
-//       pollInterval: 30000
-//     },
-//     props({ data: { partners = [], stages = [], loading } }) {
-//       return {
-//         partners,
-//         stages,
-//         loading
-//       };
-//     }
-//   })
-// )(Partners);
+      client.updateServices(services);
+      return { services: services.filter(s => s.enabled) };
+    }
+  }),
+  graphql(GET_ALL_PARTNERS, {
+    options: {
+      pollInterval: 30000
+    },
+    props({ data: { partners = [], stages = [], loading } }) {
+      return {
+        partners,
+        stages,
+        loading
+      };
+    }
+  })
+)(Partners);
