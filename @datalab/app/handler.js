aa2868733d38167ec0e35fe880df84eda8904ad4
Thomas E Lackey <telackey@bozemanpass.com>
2019-01-16 22:55:20 -0600

    Add Wallet prompt.

--- @datalab/app/handler.js
+++ @datalab/app/handler.js
@@ -29,11 +29,70 @@ export const index = Wireline.exec(async (event, context, response) => {
       <title>DataLab</title>
       <link rel="shortcut icon" href="${PUBLIC_PATH}/assets/favicon.ico">
       <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+      <style>
+         body {
+          margin: 0;
+         }
+         #needwallet {
+           font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+           font-size: 1.00rem;
+           letter-spacing: 0.0075em;
+           color: #444;
+         }
+         #needwallet #banner {
+           background-color: #3f51b5;
+           margin: 0;
+           color: #fff;
+           font-size: 1.25rem;
+           font-weight: 500;
+           line-height: 1.6;
+           padding: 1em;
+         }
+         #needwallet h1 {
+           font-size: 1.25rem;
+           font-weight: 500;
+           line-height: 1.6;
+         }
+         #needwallet a {
+          text-decoration: none;
+         }
+         #needwallet p {
+          padding-top: 0.5em;
+         }
+      </style>
+      
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script>
+        function detectWallet(onYes, onNo) {
+          if ($('#wirelineExtensionIsPresent').length) {
+            if (onYes) {
+              onYes();
+            }
+          } else {
+            if (onNo) {
+              onNo();
+            }
+          }
+        }
+
+        function addApp() {
+          $('#needwallet').remove();
+          $('body').append('<script src="${PUBLIC_PATH}/assets/app.js"></'+'script>');
+        }
+      </script>
     </head>
-    <body>
+    <body onload='detectWallet(addApp);'>
+      <div id="needwallet">
+        <div id="banner">DataLab</div>
+          <div style="padding: 1em">
+           <h1>Wireline Wallet Required</h1>
+           <p>To login, you must first install the <a href="https://github.com/wirelineio/experimental/tree/master/sub/wallet/extension">Wireline Wallet</a>
+           browser extension.</p>
+           <p><a href="https://github.com/wirelineio/experimental/tree/master/sub/wallet/extension">What is the Wireline Wallet?</a></p>
+          </div>
+      </div>
       <div id="${localConfig.rootId}"></div>
       <script>window.config = ${JSON.stringify(localConfig)};</script>
-      <script src="${PUBLIC_PATH}/assets/app.js"></script>
     </body>
     </html>
   `;
