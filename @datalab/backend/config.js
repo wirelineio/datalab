e57be4afb993650910adf4b9f201299bc616ab08
Thomas E Lackey <telackey@bozemanpass.com>
2019-02-11 12:08:09 -0600

    Fix IdM URL and realm.

--- @datalab/backend/config.js
+++ @datalab/backend/config.js
@@ -3,9 +3,9 @@ import { ServiceHelper } from '@wirelineio/sdk';
 export const IDM = {
   resourceId: ServiceHelper.mayEnv('WRL_IDM_RESOURCE_ID', 'datalab'),
 
-  attestUrl: ServiceHelper.mayEnv('WRL_IDM_URL', ServiceHelper.mayEnv('WRL_URL_ENDPOINT')),
+  attestUrl: ServiceHelper.mayEnv('WRL_IDM_URL'),
 
-  realm: ServiceHelper.mayEnv('WRL_IDM_URL', ServiceHelper.mayEnv('WRL_URL_ENDPOINT'))
+  realm: ServiceHelper.mayEnv('WRL_IDM_REALM', ServiceHelper.mayEnv('WRL_ENDPOINT_URL'))
 };
 
 export const CLAIMS = {
